5f585df6ff598fb67ca9e36d21c711745493a064
Vitor Martins <vitorfigmartins@gmail.com>
2019-07-11 16:27:11 -0300

    FInalizando tela home

--- src/components/SectionEn/index.js
+++ src/components/SectionEn/index.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 
 import './styles.css';
 
+import btnEn from '../../img/button-en.webp';
+
 export default class SectionPt extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       
+
     }
   }
 
   render() {
     return (
-      <div className="en">
+      <div className="section-home en">
+        <div>
+          <p>EN</p>
+          <h2 className="uppercase">Not sure about the size?</h2>
+          <p className="uppercase">No worries, we'll help you!</p>
+          <button className="btn" style={{ marginTop: '75px' }}>
+            <img src={btnEn} alt="Clique aqui" width='300px' />
+          </button>
+        </div>
       </div>
     )
   }
-  
+
 }
--- src/components/SectionPt/index.js
+++ src/components/SectionPt/index.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 
 import './styles.css';
 
+import btnPt from '../../img/button-pt.webp';
+
 export default class SectionPt extends Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       
+
     }
   }
 
   render() {
     return (
-      <div className="pt-br">
+      <div className="section-home pt-br">
+        <div>
+          <p>PT</p>
+          <h2 className="uppercase">Tá com dúvida no tamanho?</h2>
+          <p className="uppercase">Relaxa que a gente te ajuda!</p>
+          <button className="btn" style={{ marginTop: '75px' }}>
+            <img src={btnPt} alt="Clique aqui" width='300px' />
+          </button>
+        </div>
       </div>
     )
   }
-  
+
 }
