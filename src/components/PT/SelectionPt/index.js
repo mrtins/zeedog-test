06b8b9240bbd9711f004ba4a8974cb6e41d56203
Vitor Martins <vitorfigmartins@gmail.com>
2019-07-16 14:18:46 -0300

    Corrigindo responsividade

--- src/components/PT/SelectionPt/index.js
+++ src/components/PT/SelectionPt/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AsyncSelect from 'react-select/async';
+import {
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap';
 
 import './styles.css';
 
 import returnBtn from '../../../img/return-btn.webp';
+import disclaimerPt from '../../../img/disclaimer-br.webp';
 
 export default class SelectionPt extends Component {
   constructor(props) {
@@ -48,28 +54,37 @@ export default class SelectionPt extends Component {
   render() {
     return (
       <div className="section-home pt-br">
-        <Link to="/" className="return-btn-selection">
-          <button className="btn">
-            <img src={returnBtn} alt="Retornar para a home" width="50%" />
-          </button>
-        </Link>
-
-        <div>
-          <h3 className="title">Descubra o tamanho ideal</h3>
-          <p className="subtitle">Veja o que cachorros da mesma raça usam</p>
-        </div>
-
-        {this.state.options.length === 0 ? null :
+        <Col>
+          <Link to="/" className="return-btn-selection">
+            <button className="btn">
+              <img src={returnBtn} alt="Retornar para a home" width="50%" />
+            </button>
+          </Link>
+        </Col>
+        <Col>
           <div>
-            <AsyncSelect
-              placeholder="Selecione uma raça"
-              cacheOptions
-              defaultOptions
-              onChange={this.props.handleChange}
-              loadOptions={this.promiseOptions}
-            />
+            <h3 className="title">Descubra o tamanho ideal</h3>
+            <p className="subtitle">Veja o que cachorros da mesma raça usam</p>
           </div>
-        }
+
+          {this.state.options.length === 0 ? null :
+            <div>
+              <AsyncSelect
+                placeholder="Selecione uma raça"
+                cacheOptions
+                defaultOptions
+                onChange={this.props.handleChange}
+                loadOptions={this.promiseOptions}
+              />
+            </div>
+          }
+        </Col>
+
+        <Col>
+          <div className="disclaimer-selection">
+            <img src={disclaimerPt} alt="Se for um vira lata, escolha a raça com porte mais parecido" width="250px" style={{ marginBottom: '80px' }} />
+          </div>
+        </Col>
       </div>
     )
   }
