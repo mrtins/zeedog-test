6c52e08d36b7a0c2381137010ef10c0bc82debcb
Vitor Martins <vitorfigmartins@gmail.com>
2019-07-16 16:53:02 -0300

    Updates

---
+++ precache-manifest.f9d2fe738ddbc62ac5304fe5dadf20f0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112489803ac8e2eb83b4049eb53f43f8",
+    "url": "/mrtins.github.io/zeedog-test/index.html"
+  },
+  {
+    "revision": "0fb253090a85f49b2703",
+    "url": "/mrtins.github.io/zeedog-test/static/css/main.7a493ef6.chunk.css"
+  },
+  {
+    "revision": "7c556fffd369148fa2a5",
+    "url": "/mrtins.github.io/zeedog-test/static/js/2.23654ea3.chunk.js"
+  },
+  {
+    "revision": "0fb253090a85f49b2703",
+    "url": "/mrtins.github.io/zeedog-test/static/js/main.ae66c5a4.chunk.js"
+  },
+  {
+    "revision": "4e50e876f0e65311e523",
+    "url": "/mrtins.github.io/zeedog-test/static/js/runtime~main.95cd6f71.js"
+  },
+  {
+    "revision": "4c08bea9168bb87deb463fc9a4972df9",
+    "url": "/mrtins.github.io/zeedog-test/static/media/button-en.4c08bea9.webp"
+  },
+  {
+    "revision": "f6f2c08f4a9d3e65a6ef8d6190d64d6d",
+    "url": "/mrtins.github.io/zeedog-test/static/media/button-pt.f6f2c08f.webp"
+  },
+  {
+    "revision": "2dc8f83701c55fdb35e13e335ca36688",
+    "url": "/mrtins.github.io/zeedog-test/static/media/disclaimer-br-breed.2dc8f837.webp"
+  },
+  {
+    "revision": "dfe3185b9013db563c8d4fd7318c9a8a",
+    "url": "/mrtins.github.io/zeedog-test/static/media/disclaimer-br.dfe3185b.webp"
+  },
+  {
+    "revision": "6435f57b44302931e62640fc5f45ecaf",
+    "url": "/mrtins.github.io/zeedog-test/static/media/disclaimer-en-breed.6435f57b.webp"
+  },
+  {
+    "revision": "3ff32e6fe8ef239187a2f68927aebbd1",
+    "url": "/mrtins.github.io/zeedog-test/static/media/disclaimer-en.3ff32e6f.webp"
+  },
+  {
+    "revision": "82f8fe21b0026051725d814d19f95ff9",
+    "url": "/mrtins.github.io/zeedog-test/static/media/logo-horizontal-branco.82f8fe21.webp"
+  },
+  {
+    "revision": "dcfbcccc1160c7fe47c7b3cf5de305c9",
+    "url": "/mrtins.github.io/zeedog-test/static/media/return-btn-2-en.dcfbcccc.webp"
+  },
+  {
+    "revision": "c2ec6c6db0b77746abe5a30e615f592f",
+    "url": "/mrtins.github.io/zeedog-test/static/media/return-btn-2.c2ec6c6d.webp"
+  },
+  {
+    "revision": "3ed0da3f2e24fe3dbfef309e76db2105",
+    "url": "/mrtins.github.io/zeedog-test/static/media/return-btn-en.3ed0da3f.webp"
+  },
+  {
+    "revision": "348b4f16f66220dd46ebdb080088bb7f",
+    "url": "/mrtins.github.io/zeedog-test/static/media/return-btn.348b4f16.webp"
+  },
+  {
+    "revision": "947f7b34faffc80c2d4cc37e9f14cac3",
+    "url": "/mrtins.github.io/zeedog-test/static/media/table-background-en.947f7b34.webp"
+  },
+  {
+    "revision": "0230768e572e9098bd859b321718a8de",
+    "url": "/mrtins.github.io/zeedog-test/static/media/table-background.0230768e.webp"
+  },
+  {
+    "revision": "f456ad21273c3c97286f0d4de6a16219",
+    "url": "/mrtins.github.io/zeedog-test/static/media/td-bg-1-en.f456ad21.webp"
+  },
+  {
+    "revision": "70a7470eb39554190061a647dbec1534",
+    "url": "/mrtins.github.io/zeedog-test/static/media/td-bg-1.70a7470e.webp"
+  }
+]);
